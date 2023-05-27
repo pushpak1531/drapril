a65e8bc34739f032c42736e5d8db658fafdf88a1
iambeast07 <76482011+iambeast07@users.noreply.github.com>
2023-05-28 05:28:59 +0530

    UI components

--- src/components/BoxComponent.js
+++ src/components/BoxComponent.js
@@ -1,8 +1,14 @@
+import '../App.css'
 const BoxComponent = ({ videoUrl }) => {
   return (
-    <div className="box-container">
-      <video controls>
-        <source src={videoUrl} type="video/mp" />
+    <div className="box-container"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <video src={videoUrl} type="video/mp4" autoPlay >
         {/* Add additional video source */}
         Your Browser doesnt support video format
       </video>
