0b702000fd111b005d9a851b577571420b31a480
iambeast07 <76482011+iambeast07@users.noreply.github.com>
2023-05-28 00:55:21 +0530

    BOLDEV/feature for videocomponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react"; 
 import Particles from "react-tsparticles"; 
+import BoxComponent from "./components/BoxComponents";
 import { loadFull } from "tsparticles"; 
+import "./App.css"
 const App = () => { 
   const particlesInit = async (main) => { 
     console.log(main); 
@@ -8,6 +10,7 @@ const App = () => {
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready 
     // starting from v2 you can add only the features you need reducing the bundle size 
     await loadFull(main); 
+    // <BoxComponent videoUrl="hahah.mp4"></BoxComponent>
   }; 
   const particlesLoaded = (container) => { 
     console.log(container); 
@@ -70,6 +73,9 @@ const App = () => {
             },  
         }} 
       /> 
+      <div className="box-container">
+      <BoxComponent videoUrl="hahah.mp4"></BoxComponent>
+      </div>
     </div> 
   ); 
 }; 
